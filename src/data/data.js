5056ac6bc27e522fc11ff225ac0d2a38b5c6cb80
Franco Nicolas <franconicolas2009@live.com.ar>
2024-06-03 18:06:13 -0300

    changes

--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,32 @@
 import WorkIm from "../assets/projects/worklmg.png";
 import RealEst from "../assets/projects/1675183702635.jpg";
 import Poke from "../assets/projects/1675171571671.jpg";
+import beefson from "../assets/projects/beefson.png";
 
 export const data = [
   {
     id: 1,
+    name: "React JS Application",
+    image: beefson,
+    github: "https://github.com/odin-organization/odin",
+    live: "https://www.beesofsun.com/",
+  },
+  {
+    id: 2,
     name: "React JS Application",
     image: RealEst,
     github: "https://github.com/FrancoNicolas1/morfi",
     live: "",
   },
   {
-    id: 2,
+    id: 3,
     name: "React JS Application",
     image: Poke,
     github: "https://github.com/FrancoNicolas1/PI-Pokemon",
     live: "https://pi-pokemon-ten-pi.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "React JS Application",
     image: WorkIm,
     github: "https://github.com/FrancoNicolas1/PI-Dogs-Henry",
